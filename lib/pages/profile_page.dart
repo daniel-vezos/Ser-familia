0c816f06b7309a402ab72229b10f6106be73e319
LipeMachadoVezos <felipe.machado@vezos.com.br>
2024-08-08 13:16:17 -0300

    Add logic delete account

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -3,6 +3,7 @@ import 'package:app_leitura/pages/privacy_policy_page.dart';
 import 'package:app_leitura/pages/terms_of_use_page.dart';
 import 'package:app_leitura/widgets/button_notification.dart';
 import 'package:app_leitura/widgets/points_card.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -16,13 +17,74 @@ class ProfilePage extends StatelessWidget {
     required this.nameUser,
   });
 
+  Future<void> _deleteUser(BuildContext context) async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) return;
+
+    try {
+      // Confirme a exclusão com o usuário
+      final confirm = await showDialog<bool>(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Confirmar Exclusão'),
+          content: const Text('Tem certeza de que deseja excluir sua conta? Esta ação é irreversível.'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context, false),
+              child: const Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, true),
+              child: const Text('Excluir'),
+            ),
+          ],
+        ),
+      );
+
+      if (confirm != true) return;
+
+      // Apaga todos os dados do Firestore associados ao usuário
+      await _deleteUserData(user.uid);
+
+      // Remove o usuário
+      await user.delete();
+
+      // Logout do Firebase Auth
+      await FirebaseAuth.instance.signOut();
+
+      // Redireciona para a tela inicial após a exclusão
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => const InitialPage()),
+        (route) => false,
+      );
+    } catch (e, stackTrace) {
+      print('Erro ao excluir conta: ${e.toString()}');
+      print('StackTrace: $stackTrace');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Erro ao excluir a conta.')),
+      );
+    }
+  }
+
+
+  Future<void> _deleteUserData(String uid) async {
+    try {
+      final firestore = FirebaseFirestore.instance;
+      await firestore.collection('users').doc(uid).delete();
+      print('Dados do usuário excluídos com sucesso.');
+    } catch (e) {
+      print('Erro ao excluir dados do Firestore: ${e.toString()}');
+      rethrow;
+    }
+  }
+
   Future<void> _signOut(BuildContext context) async {
     final GoogleSignIn googleSignIn = GoogleSignIn();
 
     try {
       await googleSignIn.signOut();
       await FirebaseAuth.instance.signOut();
-      // Redireciona para a tela inicial após o logout
       Navigator.pushAndRemoveUntil(
         context,
         MaterialPageRoute(builder: (context) => const InitialPage()),
@@ -44,8 +106,7 @@ class ProfilePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          icon: Icon(Icons.arrow_back,
-              color: Colors.grey[600]), // Um cinza mais escuro
+          icon: Icon(Icons.arrow_back, color: Colors.grey[600]),
           onPressed: () => Navigator.pop(context),
         ),
         actions: [
@@ -60,11 +121,11 @@ class ProfilePage extends StatelessWidget {
             child: ButtonNotification(nameUser: nameUser),
           ),
         ],
-        backgroundColor: Colors.grey[300], // Cor de fundo da AppBar
-        elevation: 0, // Remove a sombra da AppBar
+        backgroundColor: Colors.grey[300],
+        elevation: 0,
       ),
       body: Container(
-        color: Colors.grey[300], // Define a cor de fundo
+        color: Colors.grey[300],
         padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 18),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -78,17 +139,15 @@ class ProfilePage extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) =>
-                              PrivacyPolicyPage(nameUser: nameUser)),
+                        builder: (context) => PrivacyPolicyPage(nameUser: nameUser),
+                      ),
                     );
                   },
                   child: const Row(
                     children: [
                       Text(
                         'Política de Privacidade',
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
+                        style: TextStyle(fontSize: 18),
                       ),
                     ],
                   ),
@@ -99,17 +158,15 @@ class ProfilePage extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) =>
-                              TermsOfUsePage(nameUser: nameUser)),
+                        builder: (context) => TermsOfUsePage(nameUser: nameUser),
+                      ),
                     );
                   },
                   child: const Row(
                     children: [
                       Text(
                         'Termos de Uso',
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
+                        style: TextStyle(fontSize: 18),
                       ),
                     ],
                   ),
@@ -118,14 +175,25 @@ class ProfilePage extends StatelessWidget {
             ),
             Center(
               child: ElevatedButton(
+                onPressed: () => _deleteUser(context),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xff012363),
+                  padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                  textStyle: const TextStyle(fontSize: 18),
+                ),
+                child: const Text(
+                  'Excluir Conta',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            Center(
+              child: ElevatedButton(
                 onPressed: () => _signOut(context),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: const Color(0xff012363),
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 16,
-                      horizontal: 32), // Ajusta o padding do botão
-                  textStyle: const TextStyle(
-                      fontSize: 18), // Ajusta o tamanho do texto
+                  padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                  textStyle: const TextStyle(fontSize: 18),
                 ),
                 child: const Text(
                   'Sair',
